2d104fe783f06897107830f0c7b9241604c63c8d
moshiur <moshiur71199@gmail.com>
2021-07-20 03:42:32 +0600

    Ideal Gas Simulation: Preliminary Code (need to optimize for low particle count)

---
+++ Physics/Ideal Gas Simulation/src/argument-parser.h
@@ -0,0 +1,8 @@
+#ifndef ARGUMENT_PARSER_H
+#define ARGUMENT_PARSER_H 1
+
+#include <variable-types.h>
+
+void parse_args(int argc, char *argv[], const struct SimulationParameters *restrict param);
+
+#endif
