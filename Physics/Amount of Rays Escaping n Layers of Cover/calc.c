df62bbfd3710bca74e1fe588709101e2e8776f0c
moshiur <moshiur_rahman@tutanota.com>
2023-05-07 15:03:58 +0600

    Bug fix Fixed the bug where incorrect value was given for trap_capacity when using simulation.

--- Physics/Amount of Rays Escaping n Layers of Cover/calc.c
+++ Physics/Amount of Rays Escaping n Layers of Cover/calc.c
@@ -1,54 +1,27 @@
 #include <stdlib.h>
-#include <sys/random.h>
-#include "pcg_basic.h"
 
-extern long int _num_rays;
-extern double *restrict _transparency, *restrict _trap_capacity;
-
-struct thread_data_s
-{
-	int layer_num,
-		transparency_index_min,
-		transparency_index_max;
-};
-
-// typedef-ing re-entrant random number generator data types
-typedef pcg32_random_t rand_buffer_t;
-typedef uint32_t rand_t;
-
-// initiate random number generator state buffer
-static inline void init_rand_buff(rand_buffer_t *rand_buff_ptr)
-{
-	rand_t randvar[4];
-	getentropy(randvar, 4*sizeof(rand_t));
-	pcg32_srandom_r(rand_buff_ptr, randvar[0], randvar[1]);
-	pcg32_srandom_r(rand_buff_ptr + 1, randvar[2], randvar[3]);
-
-	return;
-}
-
-// return -1 or +1 based on transparency.
-// -1 indicated the ray going to the lower layer.
-// +1 indicates the ray going to the upper layer.
-static inline int update_layer(rand_buffer_t *rand_buffer_ptr, rand_t threshold)
-{
-	return (pcg32_random_r(rand_buffer_ptr) < threshold) ? 1 : (2*(pcg32_random_r(rand_buffer_ptr + 1) & 1) - 1);
-}
+#include "defs.h"
 
 // collect how many rays hit the ground.
-static inline long int collect_rays(int last_layer, double transparency)
+static inline long int collect_rays(int last_layer, double transparency, int seed)
 {
+	struct drand48_data randbuffer;
+	srand48_r(seed, &randbuffer);
+	double rand;
+
+	enum {ground_layer = 0, first_layer = 1};
+	int layer_num;
 	long int trapped_rays = 0;
-	rand_buffer_t rand_buff[2];
-	init_rand_buff(rand_buff);
-	rand_t threshold = transparency*UINT32_MAX;
+
 	for(long int i = 0; i < _num_rays; ++i)
 	{
-		enum {ground_layer = 0, first_layer = 1};
-		int layer_num = first_layer;
+		layer_num = first_layer;
+		int direction = 1;
 		while((layer_num != ground_layer) && (layer_num != last_layer + 1))
 		{
-			layer_num += update_layer(rand_buff, threshold);
+			drand48_r(&randbuffer, &rand);
+			direction *= (2 * ( rand < transparency ) - 1);
+			layer_num +=  direction;
 		}
 		trapped_rays += (layer_num == ground_layer);
 	}
@@ -61,15 +34,16 @@ void *simulation(void *thread_data_vptr)
 	struct thread_data_s *thread_data_ptr = thread_data_vptr;
 	for(int i = thread_data_ptr->transparency_index_min; i < thread_data_ptr->transparency_index_max; ++i)
 	{
-		_trap_capacity[i] = (double)((long double)collect_rays(thread_data_ptr->layer_num, _transparency[i])/_num_rays);
+		_trap_capacity[i] = (double)collect_rays(thread_data_ptr->layer_num, _transparency[i],
+		                     rand_r(thread_data_ptr->seedp)) / _num_rays;
 	}
 
 	return NULL;
 }
 
-static inline double predict_trap_capacity(double fn, double T)
+static inline double predict_trap_capacity(double fn, double t)
 {
-	return (fn + (1 - T)*(1 - 2*fn))/(1 - (1 - T)*fn); // recursive formula
+	return 1 - (1 - fn) * t / (1 - (1 - t) * fn);
 }
 
 void *prediction(void *thread_data_vptr)
@@ -77,7 +51,7 @@ void *prediction(void *thread_data_vptr)
 	struct thread_data_s *thread_data_ptr = thread_data_vptr;
 	for(int i = thread_data_ptr->transparency_index_min; i < thread_data_ptr->transparency_index_max; ++i)
 	{
-		_trap_capacity[i] = predict_trap_capacity(_trap_capacity[i], (1 + _transparency[i])/2);
+		_trap_capacity[i] = predict_trap_capacity(_trap_capacity[i], _transparency[i]);
 	}
 
 	return NULL;
--- Physics/Amount of Rays Escaping n Layers of Cover/main.c
+++ Physics/Amount of Rays Escaping n Layers of Cover/main.c
@@ -2,25 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
-#include "pcg_basic.h"
+#include <sys/sysinfo.h>
 
-// number of threads to use for calculations.
-// set this to the no of threads in your cpu for good performance
-#define NUM_THREAD	4
-
-// structure for passing necessary data to each thread. each thread will generate
-// data points for a certain layer number and a range of transparency.
-struct thread_data_s
-{
-	int layer_num,
-		transparency_index_min,
-		transparency_index_max;
-};
+#include "defs.h"
 
 static int num_layers_min = 0, num_layers_max = 0, step_num_layers = 0, num_output_points = 0;
 
 long int _num_rays = 0;	// number of rays to simulate for each unique (layer_num, transparency).
-double *restrict _transparency = NULL, *restrict _trap_capacity = NULL;	// for holding data until all threads finish.
+double *restrict _transparency = NULL, *restrict _trap_capacity = NULL;
+int _num_threads = 1;
 
 static void init_global_vars(char **argv)
 {
@@ -44,18 +34,22 @@ static void init_global_vars(char **argv)
 	// the last element initialized explicitly so it does not exceed 1.0 due to rounding errors.
 	_transparency[num_output_points - 1] = transparency_max;
 	_trap_capacity = malloc(num_output_points*sizeof(double));
+
+	_num_threads = get_nprocs()*2;
 }
 
 // dividing work between threads (which thread will work on which range of transparency)
-static inline int gen_transparency_index_min(int thread_no) { return (thread_no*num_output_points)/NUM_THREAD; }
-static inline int gen_transparency_index_max(int thread_no) { return (++thread_no*num_output_points)/NUM_THREAD; }
+static inline int gen_transparency_index_min(int thread_no) { return (thread_no*num_output_points)/_num_threads; }
+static inline int gen_transparency_index_max(int thread_no) { return (++thread_no*num_output_points)/_num_threads; }
 
 static void gen_thread_data(char **argv,  struct thread_data_s *thread_data)
 {
-	for(int i = 0; i < NUM_THREAD; ++i)
+	for(int i = 0; i < _num_threads; ++i)
 	{
 		thread_data[i].transparency_index_min = gen_transparency_index_min(i);
 		thread_data[i].transparency_index_max = gen_transparency_index_max(i);
+		thread_data[i].seedp = malloc(sizeof(uint));
+		*thread_data[i].seedp = arc4random();
 	}
 
 	return;
@@ -78,15 +72,15 @@ static inline void print_data(double *restrict doubleptr)
 // generating (transparency, trap_capacity) [multithreaded]
 static void gen_data(void *(*thread_func)(void *), struct thread_data_s *thread_data)
 {
-	pthread_t thread[NUM_THREAD];
+	pthread_t thread[_num_threads];
 	for(int layer_num = num_layers_min; layer_num <= num_layers_max; layer_num += step_num_layers)
 	{
-		for(int i = 0; i < NUM_THREAD; ++i)
+		for(int i = 0; i < _num_threads; ++i)
 		{
 			thread_data[i].layer_num = layer_num;
 			pthread_create(&thread[i], NULL, thread_func, thread_data + i);
 		}
-		for(int i = 0; i < NUM_THREAD; ++i)
+		for(int i = 0; i < _num_threads; ++i)
 		{
 			pthread_join(thread[i], NULL);
 		}
@@ -100,7 +94,7 @@ int main(int argc, char **argv)
 {
 	init_global_vars(argv);
 
-	struct thread_data_s thread_data[NUM_THREAD];
+	struct thread_data_s thread_data[_num_threads];
 	gen_thread_data(argv,  thread_data);
 
 	print_data(_transparency);
