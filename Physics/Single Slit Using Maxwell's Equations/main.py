d7b0736058f5a1cfafaf75e485e0cffb0deaff60
moshiur <moshiur_rahman@tutanota.com>
2026-07-03 16:31:17 +0600

    modified:   main.py

--- Physics/Single Slit Using Maxwell's Equations/main.py
+++ Physics/Single Slit Using Maxwell's Equations/main.py
@@ -3,40 +3,31 @@ import jax.numpy as jnp
 from scipy.constants import c, epsilon_0, mu_0
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib import animation, colors
 
 jax.config.update('jax_enable_x64', True)
 
 def apply_boundary_cond(
-        Ex: jnp.ndarray,
-        Ey: jnp.ndarray,
-        Bz: jnp.ndarray,
-        dBz: jnp.ndarray,
+        x: jnp.ndarray,
         boundary: tuple[jnp.ndarray, int],
-        ) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+        ) -> jnp.ndarray:
 
     rows, col = boundary
-    Ex  = Ex.at[rows, col].set(0)
-    Ey  = Ey.at[rows, col].set(0)
-    Bz  = Bz.at[rows, col].set(0)
-    dBz = dBz.at[rows, col].set(0)
-
-    return Ex, Ey, Bz, dBz
+    return x.at[rows, col].set(0)
 
 def gen_initial_cond(
         D: float,
-        l: float,
+        y_max: float,
         dx: float,
         dy: float,
         wavelength: float,
-        slit_pos_rel: float ,
         a: float,
         ) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, tuple[jnp.ndarray, int]]:
 
-    x = jnp.arange(0, D + dx/2, dx)
-    y = jnp.arange(0, l + dy/2, dy)
+    x = jnp.arange(-2 * D, 1.5 * D + dx/2, dx)
+    y = jnp.arange(-y_max, y_max + dy/2, dy)
     
-    slit_index_x = int((x.size - 1) * slit_pos_rel)
+    slit_index_x = int((x.size - 1) * 4/7)
     n = int( (y.size - a / dy - 1) / 2 )
     slit_index_y = jnp.concatenate([jnp.arange(n), jnp.arange(y.size - n, y.size)])
     boundary = (slit_index_y, slit_index_x)
@@ -46,7 +37,7 @@ def gen_initial_cond(
     Ey = jnp.sin(k * x)
     Bz = Ey / c
 
-    cutoff_index = int( (0.75 * slit_pos_rel * D // wavelength) * wavelength / dx )
+    cutoff_index = int( (1.5 * D // wavelength) * wavelength / dx )
     Ey = Ey.at[:, cutoff_index:].set(0)
     Bz = Bz.at[:, cutoff_index:].set(0)
 
@@ -79,36 +70,15 @@ def iterate(
         ) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
 
     Bz += dBz
-    Ex += c**2 * ddy(Bz, dy) * dt
-    Ey += - c**2 * ddx(Bz, dx) * dt
-    dBz = (dt / 2) * (ddy(Ex, dy) - ddx(Ey, dx))
+    Ex = apply_boundary_cond(Ex + c**2 * ddy(Bz, dy) * dt, boundary)
+    Ey = apply_boundary_cond(Ey - c**2 * ddx(Bz, dx) * dt, boundary)
+    dBz = apply_boundary_cond(dt / 2 * (ddy(Ex, dy) - ddx(Ey, dx)), boundary)
     Bz += dBz
 
-    return apply_boundary_cond(Ex, Ey, Bz, dBz, boundary)
-
-def crop(
-        Ex: jnp.ndarray,
-        Ey: jnp.ndarray,
-        Bz: jnp.ndarray,
-        nx: int,
-        ny: int,
-        ratio: float,
-        ) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-
-    nrows, ncols = Ex.shape
-    startx = int(ncols * ratio)
-    stopx = ncols - startx
-    starty = int(nrows * ratio)
-    stopy = nrows - starty
-
-    Ex = Ex[starty:stopy:ny, startx:stopx:nx]
-    Ey = Ey[starty:stopy:ny, startx:stopx:nx]
-    Bz = Bz[starty:stopy:ny, startx:stopx:nx]
-
-    return Ex, Ey, Bz
+    return Ex, Ey, Bz, dBz
 
 @jax.jit
-def poynting_vector(
+def calc_poynting_vector(
         Ex: jnp.ndarray,
         Ey: jnp.ndarray,
         Bz: jnp.ndarray,
@@ -120,18 +90,21 @@ def poynting_vector(
     return Sx, Sy
 
 @jax.jit
-def energy_density(Ex: jnp.ndarray, Ey: jnp.ndarray, Bz: jnp.ndarray) -> jnp.ndarray:
+def calc_energy_density(Ex: jnp.ndarray, Ey: jnp.ndarray, Bz: jnp.ndarray) -> jnp.ndarray:
     u = epsilon_0/2 * (Ex**2 + Ey**2) + 1/(2*mu_0) * Bz**2
     return u
 
+@jax.jit
+def calc_intensity(Ey: jnp.ndarray, Bz: jnp.ndarray) -> jnp.ndarray:
+    return 1 / mu_0 * Ey * Bz
+
 wavelength = 1e-6
-D = 2 * 50 * wavelength
-dh = wavelength / 20
-l = 2 * 50 * wavelength
 a = 3 * wavelength
-dt = dh / c
-slit_pos_rel = .5
-n_iter = int(slit_pos_rel * D / c / dt)
+D = 25 * wavelength
+dh = wavelength / 40
+y_max = (1.5**2 - 1)**.5 * D
+dt = dh / c / 20
+n_iter = int(2 * D / c / dt)
 
 video_len = 10
 fps = 60
@@ -140,29 +113,59 @@ step = max(n_iter // n_frames, 1)
 poynting_vector_list = []
 energy_density_list = []
 
-Ex, Ey, Bz, boundary = gen_initial_cond(D=D, l=l, dx=dh, dy=dh, wavelength=wavelength,
-                                        slit_pos_rel=slit_pos_rel, a=a)
-dBz = (dt / 2) * ((ddy(Ex, dy=dh) - ddx(Ey, dx=dh)))
+Ex, Ey, Bz, boundary = gen_initial_cond(D=D, y_max=y_max, dx=dh, dy=dh, wavelength=wavelength, a=a)
+dBz = apply_boundary_cond(dt / 2 * (ddy(Ex, dy=dh) - ddx(Ey, dx=dh)), boundary)
 
 density = 1000
 ny, nx = Ex.shape 
 ny = max(ny // density, 1)
 nx = max(nx // density, 1)
+nrows, ncols = Ex.shape
+startx = int(ncols * 2/7)
+stopx = ncols - int(ncols * 1/7)
+starty = 0
+stopy = nrows - starty
 
+n_avg = int(10 * wavelength / c / dt)
+intensity = jnp.zeros(Ex.shape[0])
 for i in tqdm(range(n_iter)):
     Ex, Ey, Bz, dBz = iterate(Ex=Ex, Ey=Ey, Bz=Bz, dBz=dBz, dx=dh, dy=dh, dt=dt, boundary=boundary)
     if i % step == 0:
-        Ex_, Ey_, Bz_ = crop(Ex, Ey, Bz, nx, ny, ratio=.25)
+        Ex_ = Ex[starty:stopy:ny, startx:stopx:nx]
+        Ey_ = Ey[starty:stopy:ny, startx:stopx:nx]
+        Bz_ = Bz[starty:stopy:ny, startx:stopx:nx]
         # poynting_vector_list.append(poynting_vector(Ex_, Ey_, Bz_))
-        energy_density_list.append(energy_density(Ex_, Ey_, Bz_))
+        energy_density_list.append(calc_energy_density(Ex_, Ey_, Bz_))
+    if i >= n_iter - n_avg:
+        intensity += 1/n_avg * calc_intensity(Ey[starty:stopy, stopx], Bz[starty:stopy, stopx])
 
-energy_density_list = jnp.array(energy_density_list)
+jnp.save('intensity', intensity)
+jnp.save('energy_density_list', energy_density_list)
+
+# writer = animation.FFMpegWriter(fps=fps, codec='libx264')
+writer = animation.FFMpegWriter(
+        fps=fps,
+        codec="hevc_vulkan",
+        extra_args=[
+            "-init_hw_device", "vulkan=vk:0",
+            "-filter_hw_device", "vk",
+            "-vf", "format=nv12,hwupload",
+            "-qp", "22",
+            ],
+        )
 # %% -----------------------------------------------------------------------------------------------
 
+plt.close()
 fig, ax = plt.subplots()
+ax.set_xlim(-D / wavelength, D / wavelength)
+ax.set_ylim(-y_max / wavelength, y_max / wavelength)
+ax.set_xlabel(r'$x \, / \, \lambda$')
+ax.set_ylabel(r'$y \, / \, \lambda$')
+ax.axvline(x=0, ymax=.5 - a/2/y_max, linewidth=1.5, color='black')
+ax.axvline(x=0, ymin=.5 + a/2/y_max, linewidth=1.5, color='black')
 
 u = energy_density_list[0]
-im = ax.imshow(u / u.max(), cmap='Oranges', vmin=0, vmax=1)
+im = ax.imshow(u / u.max(), extent=(*ax.get_xlim(), *ax.get_ylim()), cmap='Oranges', vmin=0, vmax=1)
 
 def animate(u):
     im.set_data(u / u.max())
@@ -170,20 +173,60 @@ def animate(u):
 
 ani = animation.FuncAnimation(fig, animate, frames=energy_density_list, interval=1000/fps, blit=True)
 
-writer = animation.FFMpegWriter(fps=fps, codec='libx264')
-# writer = animation.FFMpegWriter(fps=fps, codec="hevc_vaapi", extra_args=["-vaapi_device",
-#                                                                          "/dev/dri/renderD128",
-#                                                                          "-vf",
-#                                                                          "format=nv12,hwupload"])
-
-ani.save('animation.mkv', writer=writer, dpi=600)
-plt.clf()
+ani.save('animation1.mkv', writer=writer, dpi=600)
 # %% -----------------------------------------------------------------------------------------------
 
-n_avg = int((wavelength / c) / dt)
-
+plt.close()
 fig, ax = plt.subplots()
-intensity_simulated = energy_density_list[-n_avg:, :, -1].mean(axis=0)
-ax.plot(intensity_simulated)
-plt.show()
-plt.clf()
+ax.set_xlim(0, D / wavelength)
+ax.set_ylim(-y_max / wavelength, y_max / wavelength)
+ax.set_xlabel(r'$x \, / \, \lambda$')
+ax.set_ylabel(r'$y \, / \, \lambda$')
+
+cutoff_t = int(D / 2 / c / dt / step)
+u = energy_density_list[cutoff_t]
+cutoff_x = u.shape[1] // 2
+u = u[:, cutoff_x:]
+bounds = jnp.linspace(0, 1, 1000)
+im = ax.imshow(
+        u / u.max(),
+        extent=(*ax.get_xlim(), *ax.get_ylim()),
+        cmap='Oranges',
+        # vmin=0,
+        # vmax=1,
+        # norm=colors.LogNorm(),
+        norm=colors.BoundaryNorm(boundaries=bounds, ncolors=bounds.size),
+        )
+
+def animate(u):
+    u = u[:, cutoff_x:]
+    im.set_data(u / u.max())
+    return im,
+
+ani = animation.FuncAnimation(fig, animate, frames=energy_density_list[cutoff_t: ],
+                              interval=1000/fps, blit=False)
+
+ani.save('animation2.mkv', writer=writer, dpi=600)
+# %% -----------------------------------------------------------------------------------------------
+
+plt.close()
+fig, ax = plt.subplots()
+ax.set_xlabel(r'$y \, / \, \lambda$')
+ax.set_ylabel(r'$\mathtt{I} \, / \, \mathtt{I}_0$')
+
+y = jnp.arange(-y_max, y_max + dh/2, dh)
+y = y[starty:stopy]
+
+r = jnp.sqrt(D**2 + y**2)
+sin_theta = y / r
+cos_theta = D / r
+# jnp.sinc(x) = sin(pi x)/(pi x), so this is (sin(beta)/beta)^2 with beta = pi a sin(theta)/lambda
+intensity_predicted = jnp.sinc(a / wavelength * sin_theta)**2 * cos_theta**2
+intensity_predicted /= intensity_predicted.max()
+
+ax.plot(y / wavelength, intensity / intensity.max(), label='Simulated')
+ax.plot(y / wavelength, intensity_predicted, label='Predicted')
+
+ax.legend()
+fig.savefig('intensity.pdf')
+plt.close()
